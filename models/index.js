58e31f51dd6b3bd0bdddf515bd0bd6f830acdfa6
oliver23n <oliver.nasevski@gmail.com>
2023-06-29 17:52:57 -0400

    defined assoisations between models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+  onDelete: 'SET NULL'
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag});
+Tag.belongsToMany(Product,{through: ProductTag});
 
 module.exports = {
   Product,
